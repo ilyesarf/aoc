b31f198b574ba5d0eaca6f9737393835168414cc
Quimzy <bam.boy2020@gmail.com>
2022-12-02 17:22:14 +0300

    day 2 done & refactor

--- day2.py
+++ day2.py
@@ -1,9 +1,8 @@
 from get_input import get_input
-#[point, draw, loose, win]
 
 input = get_input('2')
 
-#test_input = ["A Y\n", "B X\n", "C Z\n"]
+#input = ["A Y\n", "B X\n", "C Z\n"] #test
 
 p1 = []
 p2 = []
@@ -13,15 +12,18 @@ for i in input:
 
 
 def part1(p1, p2):
+    #[point, draw, loose, win]
     logic = {'X': [1, 'A', 'B', 'C'], 'Y': [2, 'B', 'C', 'A'], 'Z': [3, 'C', 'A', 'B']}
-
+    
     full_score = 0
     for i in range(len(p1)):
+        move1 = p1[i]
         move2 = logic[p2[i]]
+        
         score = move2[0]
-        if p1[i] == move2[1]:
+        if move1 == move2[1]:
             score += 3
-        elif p1[i] == move2[3]:
+        elif move1 == move2[3]:
             score += 6
     
         full_score += score
@@ -29,4 +31,30 @@ def part1(p1, p2):
     return full_score
 
 print(part1(p1, p2))
-      
+
+def part2(p1, p2):
+
+    #[point, draw, win, loose]
+    logic = {'A': [1, 'A', 'B', 'C'], 'B': [2, 'B', 'C', 'A'], 'C': [3, 'C', 'A', 'B']}
+    #[loose, draw, win]
+    states = ['X', 'Y', 'Z']
+
+    full_score = 0
+    for i in range(len(p1)):
+        move1 = p1[i]
+        score = states.index(p2[i])*3 #Points for state
+        
+        if p2[i] == states[0]: #loose
+            move2 = logic[move1][3]
+        elif p2[i] == states[1]: #draw
+            move2 = move1
+        elif p2[i] == states[2]: #win
+            move2 = logic[move1][2]
+        
+        score += logic[move2][0]
+        
+        full_score += score
+
+    return full_score
+
+print(part2(p1, p2))
