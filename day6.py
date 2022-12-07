fa29c4306fb7edcd176c853af8dfe35a8a599585
Quimzy <bam.boy2020@gmail.com>
2022-12-07 19:15:19 +0300

    day6 done (why didn't this commit yesterday?)

--- day6.py
+++ day6.py
@@ -3,11 +3,12 @@ from get_input import get_input
 input = get_input('6', lines=False).rstrip('\n')
 #input = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
 
-def solution(input):
+def solution(input, len_to_marker):
     for i in range(len(input)):
-        m = input[i:i+4]
-        if len(set(m)) == 4:
-            return i+4
+        m = input[i:i+len_to_marker]
+        if len(set(m)) == len_to_marker:
+            return i+len_to_marker
     
 
-print(solution(input))
+print(f"Part 1: {solution(input, 4)}")
+print(f"Part 2: {solution(input, 14)}")
